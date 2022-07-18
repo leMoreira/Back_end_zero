ee0380c4a9f07ef5832fd152c52732f03b95a3fe
unknown <leandromoreira.dev@gmail.com>
2022-07-18 14:53:51 -0300

    Interação com For e While

---
+++ estrutura_de_dados/arrays/arrays3.js
@@ -0,0 +1,41 @@
+const redesSociais = [
+    'Instagram',
+    'Youtube',
+    'Facebook',
+    'TikTok',
+    'Twitter',
+    'LinkedIn',
+    'Pinterest'
+];
+
+// for(let i = 0; i < redesSociais.length; i++){
+//     console.log(redesSociais[i]);
+// }
+
+// for(const i in redesSociais){
+//    console.log(`${i}: ${redesSociais[i]}`)
+// }
+
+// for (const valor of redesSociais){
+//     console.log(valor);
+// }
+
+// redesSociais.forEach( (valor, indice, redes) => {
+
+//     console.log(valor)
+//     console.log(indice)
+//     console.log(redes)
+
+// })
+
+let i = 0;
+while (i <redesSociais.length){
+    console.log(`${i}: ${redesSociais[i]}`)
+
+    i++;
+}
+
+
+
+
+
