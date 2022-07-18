ca8358e944d64159eac5340bb429d169361f82b3
unknown <leandromoreira.dev@gmail.com>
2022-07-17 21:17:10 -0300

    acumulador, valor

--- estrutura_de_dados/arrays/index.js
+++ estrutura_de_dados/arrays/index.js
@@ -1,6 +1,11 @@
-const saldoGabriel = 1000
-const saldoVanessa = 10000
+// const saldoGabriel = 1000
+// const saldoVanessa = 10000
+// const total = saldoGabriel + saldoVanessa
+// console.log(total)
 
-const total = saldoGabriel + saldoVanessa
+const saldoContas = [100, 200, 40, 1.90, 300, 1000, 9.99];
+// função do javascript
+
+const total = saldoContas.reduce((acumulador, valor) => acumulador + valor);
 
 console.log(total)
