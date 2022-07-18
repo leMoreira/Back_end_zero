09bd9f05a7f3cd808bbc3d4f222343799abba2a9
unknown <leandromoreira.dev@gmail.com>
2022-07-17 21:22:42 -0300

    criando um arquivo array

---
+++ estrutura_de_dados/arrays/inicio.js
@@ -0,0 +1,17 @@
+// const saldoGabriel = 1000
+// const saldoVanessa = 10000
+// const total = saldoGabriel + saldoVanessa
+// console.log(total)
+// const saldoContas = [100, 200, 40, 1.90, 300, 1000, 9.99];
+// // função do javascript
+// const total = saldoContas.reduce((acumulador, valor) => acumulador + valor);
+// console.log(total)
+
+const bandeiras = [
+    ['Brasil', ['Verde', 'Amarelo', 'Azul', 'Branco']],
+    ['canadá', ['Vermelho', 'Branco']],
+    ['México', ['Verde', 'Branco','Verde']],
+    ['EUA', ['Azul','Branco','Vermelho']]
+];
+
+console.log(bandeiras[2][1][1])
