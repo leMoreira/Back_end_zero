3446ac7c6b54e7553a3636705b5480395ede6c69
Leandro Moreira <leandromoreira.dev@gmail.com>
2022-07-25 09:07:56 -0300

    Testes com o token .. tudo ok

--- livronode/ntask-api/test/routes/token.js
+++ livronode/ntask-api/test/routes/token.js
@@ -1,27 +1,61 @@
-describe('Routes: Token', () =>{
+describe("Routes: Token", () => {
     const Users = app.models.users;
-
-    describe('POST /token', () =>{
-        beforeEach(done => {
-
-        })
-
-        describe('status 200', () =>{
-            it('returns authenticated user token', done =>{
-
-            })
-        })
-
-        describe('Status 401', () =>{
-            it('Throws error when password is incorrect', done => {
-
-            })
-            it('Throws error when email not exixts', done =>{
-
-            })
-            it('Throws error when fileds are blanks', done =>{
-                
-            })
-        })
-    })
-})
+    describe("POST /token", () => {
+      beforeEach(done => {
+        Users
+          .destroy({where: {}})
+          .then(() => Users.create({
+            name: "John",
+            email: "john@mail.net",
+            password: "12345"
+          }))
+          .then(done());
+      });
+  
+      describe("status 200", () => {
+        it("returns authenticated user token", () => {
+          request.post("/token")
+          .send({
+            email: "john@mail.net",
+            password: "12345"
+          })
+          .expect(200)
+          .end((err, res) => {
+            console.log('RES.BODY ' + expect(res.body));
+            expect(res.body).to.include.keys("token");
+            done(err);
+          })
+        });
+      });
+      
+      describe("status 401", () => {
+        it("throws error when password is incorrect", () => {
+        request.post("/token")
+          .send({
+            email: "john@mail.net",
+            password: "SENHA_ERRADA"
+          })
+          .expect(401)
+          .end((err, res) => {
+            done(err);
+          });
+        });
+        it("throws error when email not exist", () => {
+          request.post("/token")
+          .send({
+            email: "EMAIL_ERRADO",
+            password: "SENHA_ERRADA"
+          })
+          .expect(401)
+          .end((err, res) =>{
+            done(err);
+          });
+        });
+        it("throws error when email and password are blank", done => {
+          request.post("/token")
+          .expect(401)
+          .end(done);
+        });
+      });
+    });
+  });
